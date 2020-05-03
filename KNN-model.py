0d3087e77f9c46e439fe02be3ba1a011097796ff
ashtan <ashtanzk@hotmail.com>
2020-05-03 22:02:16 +0800

    K-Nearest Neighbor model

---
+++ KNN-model.py
@@ -0,0 +1,53 @@
+import sklearn
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+from sklearn import linear_model, preprocessing
+
+data = pd.read_csv("Car Data Set\car.data")
+print(data.head)
+
+# converting data to numeric data
+# i.e. low med high --> 1 2 3 etc.
+le = preprocessing.LabelEncoder()
+buying = le.fit_transform(list(data["buying"]))
+maint = le.fit_transform(list(data["maint"]))
+door = le.fit_transform(list(data["door"]))
+persons = le.fit_transform(list(data["persons"]))
+lug_boot = le.fit_transform(list(data["lug_boot"]))
+safety = le.fit_transform(list(data["safety"]))
+cls = le.fit_transform(list(data["class"]))
+
+X = list(zip(buying, maint, door, persons, lug_boot, safety))  # features
+y = list(cls)  # labels
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
+
+# training the model
+model = KNeighborsClassifier(n_neighbors=9)
+model.fit(x_train, y_train)
+acc = model.score(x_test, y_test)
+print(acc)
+
+# testing the model
+predicted = model.predict(x_test)
+names = ["unacc", "acc", "good", "vgood"]
+
+for x in range(len(predicted)):
+    print("Predicted: ", names[predicted[x]], "Data: ", x_test[x], "Actual: ", names[y_test[x]])
+
+# This will display the predicted class, our data and the actual class
+# We create a names list so that we can convert our integer predictions into
+# their string representation
+
+
+# displaying neighbors
+predicted = model.predict(x_test)
+names = ["unacc", "acc", "good", "vgood"]
+
+for x in range(len(predicted)):
+    print("Predicted: ", names[predicted[x]], "Data: ", x_test[x], "Actual: ", names[y_test[x]])
+    # Now we will we see the neighbors of each point in our testing data
+    n = model.kneighbors([x_test[x]], 9, True)
+    print("N: ", n)
